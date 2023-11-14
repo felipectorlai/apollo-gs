229969d22fe18bee785c7ec6eca355c181c4ae41
Rafael.Chaves <rafaelchaveslv@gmail.com>
2023-11-14 11:23:28 -0300

    config sass

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
+const path = require('path')
 const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+}
